e14a4ec0102757fe04f88886c1d80ee29e25864b
samiullahjamil99 <jamil.samiullah@gmail.com>
2022-04-02 18:44:53 +0500

    Added Have Customer Number Checkbox Frontend

--- woocommerce-customer-number.php
+++ woocommerce-customer-number.php
@@ -7,6 +7,12 @@ Author URI: https://www.samiullahjaml.com/
 */
 
 function custom_register_additional_fields() {
-	echo "Register Test";
+	?>
+	<p class="form-row">
+		<label class="woocommerce-form__label woocommerce-form__label-for-checkbox woocommerce-form-customer_number_present">
+			<input class="woocommerce-form__input woocommerce-form__input-checkbox" name="rememberme" type="checkbox" id="customer_number_present" value="yes" /> <span><?php esc_html_e( 'I already have my Customer Number', 'wcn' ); ?></span>
+		</label>
+	</p>
+	<?php
 }
 add_action('woocommerce_register_form','custom_register_additional_fields');
