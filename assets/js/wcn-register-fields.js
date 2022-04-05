8388325be72c1b12f6b6f01a89dc6711d10a9e41
samiullahjamil99 <jamil.samiullah@gmail.com>
2022-04-05 09:36:16 +0500

    Added Customer Number Field with Button Disabled Funtionality

--- assets/js/wcn-register-fields.js
+++ assets/js/wcn-register-fields.js
@@ -1,5 +1,18 @@
 jQuery(document).ready(function($) {
-  $(".woocommerce-form-register").on('change',function() {
-    console.log($(this)[0]);
+  $(".woocommerce-form-register").on('change input',function() {
+    var form = $(this)[0];
+    var customerNumberCheck = form.elements["customer_number_present"].checked;
+    var customerNumber = form.elements["customer_number"].value;
+    if (customerNumberCheck) {
+      $(".customer-number-container").show();
+      if (customerNumber == "" || customerNumber == null) {
+        $("button.woocommerce-form-register__submit").prop("disabled",true);
+      } else {
+        $("button.woocommerce-form-register__submit").prop("disabled",false);
+      }
+    } else {
+      $(".customer-number-container").hide();
+      $("button.woocommerce-form-register__submit").prop("disabled",false);
+    }
   });
 });
--- woocommerce-customer-number.php
+++ woocommerce-customer-number.php
@@ -12,8 +12,13 @@ function custom_register_additional_fields() {
 	?>
 	<p class="form-row">
 		<label class="woocommerce-form__label woocommerce-form__label-for-checkbox woocommerce-form-customer_number_present">
-			<input class="woocommerce-form__input woocommerce-form__input-checkbox" name="rememberme" type="checkbox" id="customer_number_present" value="yes" /> <span><?php esc_html_e( 'I already have my Customer Number', 'wcn' ); ?></span>
+			<input class="woocommerce-form__input woocommerce-form__input-checkbox" type="checkbox" name="customer_number_present" id="customer_number_present" value="yes" /> <span><?php esc_html_e( 'I already have my Customer Number', 'wcn' ); ?></span>
 		</label>
+	</p>
+	<p class="customer-number-error" style="display:none;">Error: Please Enter the Number First</p>
+	<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide customer-number-container" style="display:none;">
+		<label for="reg_customer_number"><?php esc_html_e( 'Customer Number', 'wcn' ); ?></label>
+		<input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="customer_number" id="reg_customer_number" value="<?php echo ( ! empty( $_POST['customer_number'] ) ) ? esc_attr( wp_unslash( $_POST['customer_number'] ) ) : ''; ?>" /><?php // @codingStandardsIgnoreLine ?>
 	</p>
 	<?php
 }
